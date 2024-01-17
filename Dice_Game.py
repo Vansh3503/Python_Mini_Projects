566e842addc11ccd894bae8f7ae9eb596968e1a4
Vansh Malhotra <85389767+Vansh3503@users.noreply.github.com>
2024-01-17 15:57:05 +0530

    Update Dice_Game.py

--- Dice_Game.py
+++ Dice_Game.py
@@ -2,6 +2,7 @@ import random
 print("Welcome to Dice GAme")
 dice_outcome_list=[]
 max_roll=0
+sum=0
 while max_roll<5:
     number=int(input("Enter 1 to Roll the Dice and 0 to exit:"))
 
@@ -9,8 +10,10 @@ while max_roll<5:
     if number==1:
         print("ROLLING THE DICE")
         dice_value=random.randint(1,6)
+        print(f'The Outcome is {dice_value}')
+        sum+=dice_value
         dice_outcome_list.append(dice_value)
-        max_roll=max_roll+1
+        max_roll+=1
 
     elif number==0:
         break
@@ -18,4 +21,4 @@ while max_roll<5:
         print("Invalid Input")
 
 
-print(dice_outcome_list)
+print(f'The Total Value of all Outcome is {sum}')
